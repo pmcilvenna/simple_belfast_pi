2c299029685ae3a2c1e464278c70596e11d4bf89
Phil McILvenna <pmcilvenna@magnite.com>
2025-06-27 15:25:46 +0100

    first

---
+++ src/simple_belfast_pi.cpp
@@ -0,0 +1,40 @@
+#include "simple_belfast_pi.h"
+#include <wx/dcclient.h>
+
+extern "C" opencpn_plugin* create_pi(void* ppimgr) {
+    return new simple_belfast_pi(ppimgr);
+}
+
+extern "C" void destroy_pi(opencpn_plugin* p) {
+    delete p;
+}
+
+simple_belfast_pi::simple_belfast_pi(void *ppimgr)
+    : opencpn_plugin(ppimgr) {}
+
+simple_belfast_pi::~simple_belfast_pi() {}
+
+int simple_belfast_pi::Init(void) {
+    return WANTS_OVERLAY_CALLBACK;
+}
+
+bool simple_belfast_pi::RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp) {
+    wxPoint p;
+    GetCanvasPixLL(vp, &p, lat, lon);
+    dc.SetPen(*wxRED_PEN);
+    dc.SetBrush(*wxRED_BRUSH);
+    dc.DrawCircle(p, 5);
+    return true;
+}
+
+wxString simple_belfast_pi::GetCommonName() {
+    return wxString("Simple Belfast Dot");
+}
+
+wxString simple_belfast_pi::GetShortDescription() {
+    return wxString("Displays a red dot near Belfast.");
+}
+
+wxString simple_belfast_pi::GetLongDescription() {
+    return wxString("This plugin draws a red dot near Belfast (approx. 54.6N, -5.9W).");
+}
---
+++ src/simple_belfast_pi.h
@@ -0,0 +1,24 @@
+#ifndef _SIMPLE_BELFAST_PI_H_
+#define _SIMPLE_BELFAST_PI_H_
+
+#include "ocpn_plugin.h"
+#include <wx/string.h>
+
+class simple_belfast_pi : public opencpn_plugin {
+public:
+    simple_belfast_pi(void *ppimgr);
+    ~simple_belfast_pi();
+
+    int Init(void) override;
+    bool RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp);
+
+    wxString GetCommonName() override;
+    wxString GetShortDescription() override;
+    wxString GetLongDescription() override;
+
+private:
+    double lat = 54.6;
+    double lon = -5.9;
+};
+
+#endif
