2900b3841f62b5501da4903b7eae21a897de4178
Phil McILvenna <pmcilvenna@magnite.com>
2025-07-02 15:53:14 +0100

    crusor2

--- src/simple_belfast_pi.cpp
+++ src/simple_belfast_pi.cpp
@@ -22,7 +22,7 @@ bool simple_belfast_pi::RenderOverlay(wxDC &dc, PlugIn_ViewPort *vp) {
  if (!vp) return false;  // Check for valid viewport
     wxPoint p;
 
-    GetCanvasPixLL(vp, &p, lat, lon));
+    GetCanvasPixLL(vp, &p, lat, lon);
 
     dc.SetPen(*wxRED_PEN);
     dc.SetBrush(*wxRED_BRUSH);
