ed8e44acbbcb9fe6dd4d14b3793f04f92e782013
Phil McILvenna <pmcilvenna@magnite.com>
2025-07-03 15:19:30 +0100

    add build folder

---
+++ build/src/CMakeFiles/simple_belfast_pi.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/pmcilvenna/opencpn-plugins/simple_belfast_pi_github_project/simple_belfast_pi_github/src/simple_belfast_pi.cpp" "src/CMakeFiles/simple_belfast_pi.dir/simple_belfast_pi.cpp.o" "gcc" "src/CMakeFiles/simple_belfast_pi.dir/simple_belfast_pi.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
