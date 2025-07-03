416baa49825e538832c6da9d5f987140dac9cdf8
Phil McILvenna <pmcilvenna@magnite.com>
2025-07-03 10:54:25 +0100

    work4

--- src/simple_belfast_pi.cpp
+++ src/simple_belfast_pi.cpp
@@ -1,20 +1,21 @@
 #include "simple_belfast_pi.h"
 #include <wx/dcclient.h>
 
-extern "C" opencpn_plugin* create_pi(void* ppimgr) {
+extern "C" DECL_EXP opencpn_plugin* create_pi(void* ppimgr) {
     return new simple_belfast_pi(ppimgr);
 }
 
-extern "C" void destroy_pi(opencpn_plugin* p) {
+extern "C" DECL_EXP void destroy_pi(opencpn_plugin* p) {
     delete p;
 }
 
 simple_belfast_pi::simple_belfast_pi(void *ppimgr)
-    : opencpn_plugin(ppimgr) {}
+    : opencpn_plugin_117 (ppimgr) {}
 
-simple_belfast_pi::~simple_belfast_pi() {}
+simple_belfast_pi::~simple_belfast_pi(void) {}
 
 int simple_belfast_pi::Init(void) {
+ AddLocaleCatalog("opencpn-belfast_pi");
     return WANTS_OVERLAY_CALLBACK;
 }
 
